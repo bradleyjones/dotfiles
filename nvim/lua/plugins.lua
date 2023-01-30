01589bd12e8bd7dbf2915da29696963ff82868af
Bradley Jones <jones.bradley@me.com>
2023-01-30 09:01:42 +0000

    chore: switch telescope to use release branch rather than tag
    Signed-off-by: Bradley Jones <jones.bradley@me.com>

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -26,8 +26,7 @@ return require('packer').startup({function(use)
 
   -- Fuzzy Finder
   use {
-  'nvim-telescope/telescope.nvim', tag = '0.1.0',
--- or                            , branch = '0.1.x',
+  'nvim-telescope/telescope.nvim', branch = '0.1.x',
   requires = { {'nvim-lua/plenary.nvim'} }
 }
 
