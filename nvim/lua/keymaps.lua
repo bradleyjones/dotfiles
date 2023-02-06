9ba5aa91444ea33bc313caf302912d3c4334c6f1
Bradley Jones <jones.bradley@me.com>
2023-02-06 20:43:36 +0000

    feat: add telescope keymaps to match that of vim fzf config
    Signed-off-by: Bradley Jones <jones.bradley@me.com>

---
+++ nvim/lua/keymaps.lua
@@ -0,0 +1,10 @@
+-- Key maps --
+--
+-- Telescope
+local telescope = require('telescope.builtin')
+vim.keymap.set('n', '<C-f>f', telescope.find_files, {})
+vim.keymap.set('n', '<C-f>g', telescope.git_files, {})
+vim.keymap.set('n', '<C-g>', telescope.live_grep, {}) -- live grep uses ripgrep
+vim.keymap.set('n', '<C-s>', telescope.resume, {})
+vim.keymap.set('n', '<C-b>', telescope.buffers, {})
+vim.keymap.set('n', '<leader>?', telescope.help_tags, {})
