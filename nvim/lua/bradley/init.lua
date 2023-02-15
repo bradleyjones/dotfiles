9e800aacb3f4bbdd729c6dbcd43e534461cc8f5a
Bradley Jones <jones.bradley@me.com>
2023-02-15 23:51:39 +0000

    feat: some sane vim setting defaults
    Signed-off-by: Bradley Jones <jones.bradley@me.com>

--- nvim/lua/bradley/init.lua
+++ nvim/lua/bradley/init.lua
@@ -1,4 +1,32 @@
+-- Include these modules
+
 require('bradley.plugins')
 require('bradley.keymaps')
 
+--
+-- Vim settings
+--
+
 vim.opt.termguicolors = true
+
+-- Line Number Column
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "yes"
+
+-- Tabs
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+-- Indentation
+vim.opt.smartindent = true
+
+-- Swapfile/Backup
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+-- Scrolling
+vim.opt.scrolloff = 8
+vim.opt.updatetime = 50
