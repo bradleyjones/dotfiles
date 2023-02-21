795a7060437aeed6ddb4bedd1bb148569c5ac6de
Bradley Jones <jones.bradley@me.com>
2023-02-21 00:49:01 +0000

    feat: add neovim lsp plugins and basic config
    Signed-off-by: Bradley Jones <jones.bradley@me.com>

--- nvim/lua/bradley/plugins.lua
+++ nvim/lua/bradley/plugins.lua
@@ -72,6 +72,31 @@ return require('packer').startup({function(use)
 		end
 	})
 
+	-- LSP
+	use({
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v1.x',
+		requires = {
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},             -- Required
+			{'williamboman/mason.nvim'},           -- Optional
+			{'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+			-- Autocompletion
+			{'hrsh7th/nvim-cmp'},         -- Required
+			{'hrsh7th/cmp-nvim-lsp'},     -- Required
+			{'hrsh7th/cmp-buffer'},       -- Optional
+			{'hrsh7th/cmp-path'},         -- Optional
+			{'saadparwaiz1/cmp_luasnip'}, -- Optional
+			{'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+			-- Snippets
+			{'L3MON4D3/LuaSnip'},             -- Required
+			{'rafamadriz/friendly-snippets'}, -- Optional
+		},
+		config = function() require('bradley.plugin-configs.lsp') end
+	})
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if packer_bootstrap then
