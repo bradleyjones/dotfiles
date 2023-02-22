b9e3f99a0e196df06a309e1fb437a9c88c0f568d
Bradley Jones <jones.bradley@me.com>
2023-02-22 16:00:37 +0000

    chore: update list of default installed treesitter parsers
    Signed-off-by: Bradley Jones <jones.bradley@me.com>

--- nvim/lua/bradley/plugin-configs/treesitter.lua
+++ nvim/lua/bradley/plugin-configs/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
 	-- A list of parser names
-	ensure_installed = { "c", "lua", "vim", "help" },
+	ensure_installed = { "c", "lua", "vim", "help", "yaml", "json", "go", "python", "bash" },
 
 	-- Install parsers synchronously (only applied to `ensure_installed`)
 	sync_install = false,
