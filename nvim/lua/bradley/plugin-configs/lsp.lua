38d028768cf53e4014852fcb5603ed7f6bafdc87
Bradley Jones <bradley.jones@anchore.com>
2023-08-31 15:00:43 +0100

    feat: add golangci-lint-langserver lsp
    Signed-off-by: Bradley Jones <bradley.jones@anchore.com>

--- nvim/lua/bradley/plugin-configs/lsp.lua
+++ nvim/lua/bradley/plugin-configs/lsp.lua
@@ -57,6 +57,25 @@ lsp.configure('pylsp', {
 	}
 })
 
+-- Golang LSP
+local lspconfig = require 'lspconfig'
+local configs = require 'lspconfig/configs'
+
+if not configs.golangcilsp then
+ 	configs.golangcilsp = {
+		default_config = {
+			cmd = {'golangci-lint-langserver'},
+			root_dir = lspconfig.util.root_pattern('.git', 'go.mod'),
+			init_options = {
+					command = { "golangci-lint", "run", "--out-format", "json", "--issues-exit-code=1" };
+			}
+		};
+	}
+end
+lspconfig.golangci_lint_ls.setup {
+	filetypes = {'go','gomod'}
+}
+
 -- Key mappings need to be done here to apply per buffer
 lsp.on_attach(function(_, bufnr)
 	local opts = { buffer = bufnr, remap = false }
