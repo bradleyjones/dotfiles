240cc792042ddc4d487f816cec5658c39face913
Bradley Jones <bradley.jones@anchore.com>
2023-10-19 11:30:02 +0100

    fix: stop auto closing trouble window on changes
    Signed-off-by: Bradley Jones <bradley.jones@anchore.com>

--- nvim/lua/bradley/keymaps.lua
+++ nvim/lua/bradley/keymaps.lua
@@ -37,12 +37,12 @@ vim.keymap.set('i', '<C-k>', '<C-o>k')
 vim.keymap.set('i', '<C-l>', '<C-o>l')
 --
 -- Trouble
-vim.keymap.set('n', 'gr', '<cmd>TroubleToggle lsp_references<cr>', { silent = true, noremap = true })
+vim.keymap.set('n', 'gr', '<cmd>Trouble lsp_references<cr>', { silent = true, noremap = true })
 vim.keymap.set('n', '<leader>tt', '<cmd>TroubleToggle<cr>', { silent = true, noremap = true })
-vim.keymap.set('n', '<leader>tw', '<cmd>TroubleToggle workspace_diagnostics<cr>', { silent = true, noremap = true })
-vim.keymap.set('n', '<leader>td', '<cmd>TroubleToggle document_diagnostics<cr>', { silent = true, noremap = true })
-vim.keymap.set('n', '<leader>tl', '<cmd>TroubleToggle loclist<cr>', { silent = true, noremap = true })
-vim.keymap.set('n', '<leader>tq', '<cmd>TroubleToggle quickfix<cr>', { silent = true, noremap = true })
+vim.keymap.set('n', '<leader>tw', '<cmd>Trouble workspace_diagnostics<cr>', { silent = true, noremap = true })
+vim.keymap.set('n', '<leader>td', '<cmd>Trouble document_diagnostics<cr>', { silent = true, noremap = true })
+vim.keymap.set('n', '<leader>tl', '<cmd>Trouble loclist<cr>', { silent = true, noremap = true })
+vim.keymap.set('n', '<leader>tq', '<cmd>Trouble quickfix<cr>', { silent = true, noremap = true })
 --
 -- NeoTest
 local neotest = require('neotest')
