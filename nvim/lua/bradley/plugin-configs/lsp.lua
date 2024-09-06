89dfce3fb4109324158840511c285c8acf8015f8
Bradley Jones <bradley.jones@anchore.com>
2024-09-06 22:36:07 +0100

    feat: switch from packer to lazy and update lsp-zero
    Signed-off-by: Bradley Jones <bradley.jones@anchore.com>

--- nvim/lua/bradley/plugin-configs/lsp.lua
+++ nvim/lua/bradley/plugin-configs/lsp.lua
@@ -13,16 +13,36 @@ require('mason.settings').set({
 
 local lsp = require('lsp-zero')
 
-lsp.preset({
-	name = 'minimal',
-	set_lsp_keycaps = true,
-	manage_nvim_cmp = false,
-	suggest_lsp_servers = true,
+-- lsp_attach is where you enable features that only work
+-- if there is a language server active in the file
+local lsp_attach = function(client, bufnr)
+	local opts = { buffer = bufnr, remap = false }
+
+	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+	vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+	vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+	vim.keymap.set("n", "gR", function() vim.lsp.buf.rename() end, opts)
+	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+end
+
+
+lsp.extend_lspconfig({
+  sign_text = true,
+  lsp_attach = lsp_attach,
+  capabilities = require('cmp_nvim_lsp').default_capabilities(),
 })
 
-lsp.ensure_installed({
-	'lua_ls',
-	'gopls',
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  -- Replace the language servers listed here
+  -- with the ones you want to install
+  ensure_installed = {'lua_ls', 'gopls', 'pylsp'},
+  handlers = {
+    function(server_name)
+      require('lspconfig')[server_name].setup({})
+    end,
+  }
 })
 
 -- Fix Undefined global 'vim'
@@ -76,17 +96,6 @@ lspconfig.golangci_lint_ls.setup {
 	filetypes = {'go','gomod'}
 }
 
--- Key mappings need to be done here to apply per buffer
-lsp.on_attach(function(_, bufnr)
-	local opts = { buffer = bufnr, remap = false }
-
-	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-	vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-	vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-	vim.keymap.set("n", "gR", function() vim.lsp.buf.rename() end, opts)
-	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-end)
 
 -- Finalise LSP Setup
 lsp.setup()
@@ -97,41 +106,28 @@ lsp.setup()
 vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
 
 local cmp = require('cmp')
-local cmp_select_opts = { behavior = cmp.SelectBehavior.Select }
-local cmp_config = lsp.defaults.cmp_config({
-	snippet = {
-		expand = function(args)
-			require('luasnip').lsp_expand(args.body)
-		end,
-	},
-	window = {
-		completion = cmp.config.window.bordered()
-	},
-	experimental = { ghost_text = false },
-	sources = {
-		{ name = 'path' },
-		{ name = 'nvim_lsp',               keyword_length = 1 },
-		{ name = 'buffer',                 keyword_length = 3 },
-		{ name = 'luasnip',                keyword_length = 2 },
-		{ name = 'nvim_lsp_signature_help' }
-	},
-	mapping = cmp.mapping.preset.insert({
-		['<Tab>'] = cmp.mapping.confirm({
-			behavior = cmp.ConfirmBehavior.Replace,
-			select = true,
-		}),
-		['<CR>'] = cmp.mapping.close(),
-		['<C-e>'] = cmp.mapping.close(),
-		['<C-p>'] = cmp.mapping.select_prev_item(cmp_select_opts),
-		['<C-n>'] = cmp.mapping.select_next_item(cmp_select_opts),
-	}),
+
+cmp.setup({
+  sources = {
+	{ name = 'path' },
+	{ name = 'nvim_lsp',               keyword_length = 1 },
+	{ name = 'buffer',                 keyword_length = 3 },
+	{ name = 'luasnip',                keyword_length = 2 },
+	{ name = 'nvim_lsp_signature_help' }
+  },
+  snippet = {
+    expand = function(args)
+      -- You need Neovim v0.10 to use vim.snippet
+      vim.snippet.expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({}),
 })
 
-cmp.setup(cmp_config)
 
 -- Diagnostic messages
 vim.diagnostic.config({
-	virtual_lines = false, -- enable/disable lsp_lines (default false so enabled with toggle
+	virtual_lines = false, -- enable/disable lsp_lines (default false so enabled with toggle)
 	virtual_text = {
 		prefix = '',
 		spacing = 16,
