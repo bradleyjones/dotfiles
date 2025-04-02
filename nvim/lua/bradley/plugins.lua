9b75da8d064dbc5594d6caf117ace2bbee6ea46b
Bradley Jones <bradley.jones@anchore.com>
2025-04-02 14:42:22 +0100

    feat: change to gpt-4o-mini
    Signed-off-by: Bradley Jones <bradley.jones@anchore.com>

--- nvim/lua/bradley/plugins.lua
+++ nvim/lua/bradley/plugins.lua
@@ -401,7 +401,17 @@ require("lazy").setup({
 		{
 			"jackMort/ChatGPT.nvim",
 			config = function()
-				require("chatgpt").setup()
+				require("chatgpt").setup({
+					openai_params = {
+						model = "gpt-4o-mini",
+						frequency_penalty = 0,
+						presence_penalty = 0,
+						max_tokens = 4095,
+						temperature = 0.2,
+						top_p = 0.1,
+						n = 1,
+					},
+				})
 			end,
 			dependencies = {
 				"MunifTanjim/nui.nvim",
